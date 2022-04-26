2fefaa25d06e0641ca3619a71cb0445251c13699
31159piko-suke <k426.31159@gmail.com>
2022-04-26 23:16:11 +0900

    don't use put url

--- s3.py
+++ s3.py
@@ -1,37 +1,32 @@
 import boto3
 from botocore.client import Config
-import requests
 
 
 def generate_presignedurl(user: str, ts: str):
-    bucket = "shogi-slack"
+    S3_BUCKET_NAME = "shogi-slack"
     s3path = f"{user}/{ts}.jpg"
 
     s3 = boto3.client("s3", config=Config(signature_version="s3v4"))
-    put_url = s3.generate_presigned_url(
-        ClientMethod="put_object",
-        Params={
-            "Bucket": bucket,
-            "Key": s3path,
-        },
-        ExpiresIn=3600,
-        HttpMethod="PUT",
-    )
-
     get_url = s3.generate_presigned_url(
         ClientMethod="get_object",
         Params={
-            "Bucket": bucket,
+            "Bucket": S3_BUCKET_NAME,
             "Key": s3path,
         },
         ExpiresIn=43200,
         HttpMethod="GET",
     )
-    return put_url, get_url
+    return get_url
 
 
 def put_s3_and_get_url(user: str, ts: str):
-    puturl, geturl = generate_presignedurl(user, ts)
+    S3_BUCKET_NAME = "shogi-slack"
+    s3path = f"{user}/{ts}.jpg"
+    s3 = boto3.resource("s3")
+    obj = s3.Object(S3_BUCKET_NAME, s3path)
     img_bytes = open(f"/tmp/{user}:{ts}.jpg", "rb").read()
-    requests.put(url=puturl, data=img_bytes)
+    obj.put(Body=img_bytes)
+
+    geturl = generate_presignedurl(user, ts)
+
     return geturl
