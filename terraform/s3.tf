0834b38779778ccc31708084b410f91252fd2854
31159piko-suke <k426.31159@gmail.com>
2022-06-08 21:15:18 +0900

    add s3 bucket to use oauth

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -43,6 +43,66 @@ resource "aws_s3_bucket_public_access_block" "slack_shogi" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket" "slack_shogi_installation" {
+  bucket = "slack-shogi-installation"
+  tags = {
+    Project  = var.project,
+    Resource = "s3"
+  }
+}
+
+resource "aws_s3_bucket_acl" "slack_shogi_installation" {
+  bucket = aws_s3_bucket.slack_shogi_installation.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "slack_shogi_installation" {
+  bucket = aws_s3_bucket.slack_shogi_installation.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "slack_shogi_installation" {
+  bucket                  = aws_s3_bucket.slack_shogi_installation.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "slack_shogi_state" {
+  bucket = "slack-shogi-state"
+  tags = {
+    Project  = var.project,
+    Resource = "s3"
+  }
+}
+
+resource "aws_s3_bucket_acl" "slack_shogi_state" {
+  bucket = aws_s3_bucket.slack_shogi_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "slack_shogi_state" {
+  bucket = aws_s3_bucket.slack_shogi_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "slack_shogi_state" {
+  bucket                  = aws_s3_bucket.slack_shogi_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 
 # --------------------
 # S3 IAM Policy
@@ -50,7 +110,9 @@ resource "aws_s3_bucket_public_access_block" "slack_shogi" {
 resource "aws_iam_policy" "s3_put" {
   name = "${var.project}_S3Put"
   policy = templatefile("iam_policy_documents/S3PutPolicy.json", {
-    bucket_name = aws_s3_bucket.slack_shogi.bucket
+    bucket_name         = aws_s3_bucket.slack_shogi.bucket
+    bucket_installation = aws_s3_bucket.slack_shogi_installation.bucket
+    bucket_state        = aws_s3_bucket.slack_shogi_state.bucket
   })
   tags = {
     Project  = var.project,
